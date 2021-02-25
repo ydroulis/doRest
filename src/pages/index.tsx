1c00467ce212626048fa0c6c7add7515932aa4f5
ydrouulis <lombardi.droulis@gmail.com>
2021-02-25 00:07:22 -0300

    feat(pages): add challenge box

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,9 @@ import { CompletedChallenges } from "../components/CompletedChallenges";
 import { Countdown } from "../components/Countdown";
 import { ExperienceBar } from "../components/ExperienceBar";
 import { Profile } from "../components/Profile";
-import styles from '../styles/pages/Home.module.css'
+import { ChallengeBox } from "../components/ChallengeBox";
+
+import styles from '../styles/pages/Home.module.css';
 
 import Head from 'next/head'
 
@@ -21,7 +23,7 @@ export default function Home() {
           <Countdown/>
         </div>
         <div>
-
+          <ChallengeBox/>
         </div>
       </section>
     </div>
