639d5330ef3fddf0ce71e9f3ace0a7606c0cfe9f
ydrouulis <lombardi.droulis@gmail.com>
2021-02-25 00:05:37 -0300

    refac(components/experiencebar): use context

--- src/components/ExperienceBar.tsx
+++ src/components/ExperienceBar.tsx
@@ -1,14 +1,21 @@
+import { useContext } from 'react'
+import { ChallengesContexts } from '../contexts/ChallengesContexts'
 import styles from '../styles/components/ExperienceBar.module.css'
 
 export function ExperienceBar() {
+    const {currentExperience, experienceToNextLevel } = useContext(ChallengesContexts)
+
+    const percentToNextLevel = Math.round(currentExperience * 100) / experienceToNextLevel;
+    console.log(percentToNextLevel);
+
     return(
         <header className={styles.experienceBar}>
             <span>0 xp</span>
             <div>
-                <div style={{ width: "50%"}}/>
-                <span className={styles.currentExperience} style={{left: '50%'}}>300 xp</span>
+                <div style={{ width: `${percentToNextLevel}%`}}/>
+                <span className={styles.currentExperience} style={{left: `${percentToNextLevel}%`}}>{currentExperience} xp</span>
             </div>
-            <span>600 xp</span>
+            <span>{experienceToNextLevel} xp</span>
         </header>
     )
 }
