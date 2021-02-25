c883f1650b27a50dc297f7f8e79429b94786475f
ydrouulis <lombardi.droulis@gmail.com>
2021-02-25 11:36:14 -0300

    feat(components): use context

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,6 +1,10 @@
+import { useContext } from 'react'
+import { ChallengesContexts } from '../contexts/ChallengesContexts'
 import styles from '../styles/components/Profile.module.css'
 
 export function Profile() {
+    const { level } = useContext(ChallengesContexts)
+
     return (
         <div className={styles.profileContainer}>
             <img src="https://github.com/ydroulis.png" alt="Profile img" />
@@ -8,7 +12,7 @@ export function Profile() {
                 <strong>Yuri Lombardi Androulis</strong>
                 <p>
                     <img src="icons/level.svg" alt="Icon level"/>
-                    Level 1
+                    Level {level}
                 </p>
             </div>
         </div>
