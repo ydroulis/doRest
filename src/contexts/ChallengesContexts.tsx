b940babd3bb92c11390153351bc2daaa1049bb32
ydrouulis <lombardi.droulis@gmail.com>
2021-02-27 11:07:05 -0300

    chore(contexts): remove notification on mobile

--- src/contexts/ChallengesContexts.tsx
+++ src/contexts/ChallengesContexts.tsx
@@ -2,6 +2,7 @@ import { createContext, useState, ReactNode, useEffect } from 'react'
 import Cookies from 'js-cookie'
 import challenges from '../../challenges.json'
 import { LevelUpModal } from '../components/LevelUpModal';
+import { isMobile } from 'react-device-detect';
 
 interface Challenge {
     type: 'body' | 'eye';
@@ -72,7 +73,7 @@ export function ChallengesProvider({
 
         new Audio('/notification.mp3').play();
 
-        if (Notification.permission == 'granted') {
+        if ( !isMobile && Notification.permission == 'granted') {
             new Notification('Novo desafio 🎉', {
                 body: `Valendo ${challenge.amount}xp`
             })
